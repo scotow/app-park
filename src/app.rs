fb03adbe8f181447c6222b309cbd39313e4077ee
Scotow <contact@scotow.com>
2021-09-30 15:00:11 +0200

    Add manifest generation, front app listing, last modified metadata

--- src/app.rs
+++ src/app.rs
@@ -4,24 +4,29 @@ use zip::ZipArchive;
 use std::io::{Read, Cursor};
 use plist::Value;
 use serde::Serialize;
+use chrono::{Utc, TimeZone, DateTime};
 
 #[derive(Clone, Serialize, Debug)]
 pub struct App {
     id: String,
+    bundle_id: String,
     name: String,
     version: String,
     build: String,
-    image: Option<String>,
+    date: DateTime<Utc>,
+    icon: Option<String>,
 }
 
 impl App {
     pub fn new(path: PathBuf) -> (String, Self) {
         let mut plist = None;
-        let mut image: Option<Vec<u8>> = None;
+        let mut date = None;
+        let mut icon: Option<Vec<u8>> = None;
 
         let file = File::open(&path).unwrap();
         let mut archive = ZipArchive::new(file).unwrap();
         for i in 0..archive.len() {
+
             let mut zip_file = archive.by_index(i).unwrap();
             let path = Path::new(zip_file.name());
             let components = path.components().map(|c| c.as_os_str().to_str().unwrap()).collect::<Vec<_>>();
@@ -33,37 +38,52 @@ impl App {
                 let mut data = Vec::with_capacity(zip_file.size() as usize);
                 zip_file.read_to_end(&mut data).unwrap();
                 plist = Some(data);
+            } else if components[2] == components[1].trim_end_matches(".app") {
+                let modified = zip_file.last_modified();
+                date = Some(Utc.ymd(modified.year() as i32, modified.month() as u32, modified.day() as u32)
+                    .and_hms(modified.hour() as u32, modified.minute() as u32, modified.second() as u32));
             } else if components[2].starts_with("AppIcon") && components[2].ends_with(".png") {
-                if image.is_none() || (zip_file.size() < 32_000 && image.as_ref().map(|i| zip_file.size() > i.len() as u64) == Some(true)) {
+                if icon.is_none() || (zip_file.size() < 32_000 && icon.as_ref().map(|i| zip_file.size() > i.len() as u64) == Some(true)) {
                     let mut data = Vec::with_capacity(zip_file.size() as usize);
                     zip_file.read_to_end(&mut data).unwrap();
-                    image = Some(data);
+                    icon = Some(data);
                 }
             }
         }
 
-        if let Some(plist) = plist {
-            let plist = Value::from_reader(Cursor::new(plist)).unwrap();
-            let dict = plist.as_dictionary().unwrap();
+        match (plist, date) {
+            (Some(plist), Some(date)) => {
+                let plist = Value::from_reader(Cursor::new(plist)).unwrap();
+                let dict = plist.as_dictionary().unwrap();
 
-            let version = dict.get("CFBundleShortVersionString").unwrap().as_string().unwrap();
-            let build = dict.get("CFBundleVersion").unwrap().as_string().unwrap();
-            let name = dict.get("CFBundleDisplayName")
-                .or_else(|| dict.get("CFBundleName"))
-                .unwrap()
-                .as_string()
-                .unwrap();
+                let bundle = dict.get("CFBundleIdentifier").unwrap().as_string().unwrap();
+                let version = dict.get("CFBundleShortVersionString").unwrap().as_string().unwrap();
+                let build = dict.get("CFBundleVersion").unwrap().as_string().unwrap();
+                let name = dict.get("CFBundleDisplayName")
+                    .or_else(|| dict.get("CFBundleName"))
+                    .unwrap()
+                    .as_string()
+                    .unwrap();
 
-            let id = path.file_stem().unwrap().to_str().unwrap().to_owned();
-            (id.clone(), App {
-                id,
-                name: name.to_owned(),
-                version: version.to_owned(),
-                build: build.to_owned(),
-                image: image.map(|i| base64::encode(&i)),
-            })
-        } else {
-            panic!("")
+                let id = path.file_stem().unwrap().to_str().unwrap().to_owned();
+                (id.clone(), App {
+                    id,
+                    bundle_id: bundle.to_owned(),
+                    name: name.to_owned(),
+                    version: version.to_owned(),
+                    build: build.to_owned(),
+                    date,
+                    icon: icon.map(|i| base64::encode(&i)),
+                })
+            },
+            _ => panic!(""),
         }
+    }
+
+    pub fn manifest(&self) -> String {
+        include_str!("assets/manifest.plist")
+            .replace("$BUNDLE_IDENTIFIER", &self.bundle_id)
+            .replace("$BUNDLE_VERSION", &self.version)
+            .replace("$TITLE", &self.name)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use std::collections::HashMap;
 use std::sync::Arc;
 
 use axum::{AddExtensionLayer, handler::get, Router, Json};
-use axum::extract::Extension;
+use axum::extract::{Extension, Path};
 use tokio::sync::Mutex;
 
 use crate::app::App;
+use axum::response::Html;
+use axum::http::{HeaderMap, HeaderValue};
+use axum::http::header::CONTENT_TYPE;
 
 mod app;
 
@@ -16,7 +19,10 @@ async fn main() {
     let apps = Arc::new(Mutex::new(find_apps().await));
 
     let app = Router::new()
+        .route("/", get(index))
+        .route("/index.html", get(index))
         .route("/apps", get(list_apps))
+        .route("/apps/:id/manifest", get(app_manifest))
         .layer(AddExtensionLayer::new(apps));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -25,8 +31,18 @@ async fn main() {
         .unwrap();
 }
 
-async fn list_apps(state: Extension<Apps>) -> Json<Vec<App>> {
-    Json(state.0.lock().await.values().cloned().collect())
+async fn index() -> Html<&'static str> {
+    Html(include_str!("assets/index.html"))
+}
+
+async fn list_apps(Extension(apps): Extension<Apps>) -> Json<Vec<App>> {
+    Json(apps.lock().await.values().cloned().collect())
+}
+
+async fn app_manifest(Path(id): Path<String>, Extension(apps): Extension<Apps>) -> (HeaderMap, String) {
+    let mut headers = HeaderMap::new();
+    headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/xml"));
+    (headers, apps.lock().await.get(&id).unwrap().manifest())
 }
 
 async fn find_apps() -> HashMap<String, App> {
